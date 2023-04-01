b85e0e990fcd8cb3ed263994867e816d90d2ac57
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-04-01 20:01:44 +0530

    App.js Updated and Why section added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './components/Home';
 import Studies from './components/Studies';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Why_sec from './components/Why_sec';
 function App() {
   return (
     <>
     <Header/>
     <Home/>
+    <Why_sec/>
     <Studies/>
     <Contact/>
     <Footer/>
