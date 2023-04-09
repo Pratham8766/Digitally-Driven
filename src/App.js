21d99005acc2d1c7a73d01020c08791b363a349e
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-04-09 20:39:40 +0530

    React Router dom added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import Home from './components/Home';
-import Studies from './components/Studies';
+// import Home from './components/Home';
+// import Why_sec from './components/Why_sec';
+// import Studies from './components/Studies';
+// import Contact from './components/Contact';
+import About from "./components/About";
 import Footer from './components/Footer';
-import Contact from './components/Contact';
-import Why_sec from './components/Why_sec';
+import MainLayout from './components/MainLayout';
+import Services from './components/Services';
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <Why_sec/>
-    <Studies/>
-    <Contact/>
-    <Footer/>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>  
     </>
   );
 }
