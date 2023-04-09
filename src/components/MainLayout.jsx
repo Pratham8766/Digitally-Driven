9b33e89abc1da7f202f94b0e3b5154ec5abfdbcd
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-04-09 20:40:47 +0530

    Main Layout created
    Load the home page of the website

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from "../components/Home";
+import Why_sec from "../components/Why_sec";
+import Studies from "../components/Studies";
+import Contact from "../components/Contact";
+
+export default function MainLayout() {
+  return (
+    <>
+    <Home/>
+    <Why_sec/>
+    <Studies/>
+    <Contact/>
+    </>
+  )
+}
